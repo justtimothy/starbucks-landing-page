2de2346df3ade54fea0fd7b4da3cdb6e8dc56c40
TIMOTHY HARAMI ADAMU <56508971+justtimothy@users.noreply.github.com>
2023-10-03 09:32:17 +0100

    Add files via upload

---
+++ STARBUCKS LANDING PAGE/js/app.js
@@ -0,0 +1,8 @@
+function imgSlider(ssaa) {
+    document.querySelector('.starbucks').src = ssaa;
+} 
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+
+}
